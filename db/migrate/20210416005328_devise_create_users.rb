5d2c4bd85750c52b287cb7bfeaf506e79394d9f7
jtellis1991 <joshuaellis1991@gmail.com>
2021-04-16 01:18:42 +0000

    Created usere table

--- db/migrate/20210416005328_devise_create_users.rb
+++ db/migrate/20210416005328_devise_create_users.rb
@@ -2,11 +2,10 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
+    enable_extension("citext")
     create_table :users do |t|
-      enable_extension("citext")
-
       ## Database authenticatable
-      t.citext :email,              null: false, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -34,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
+      t.citext :username
       t.boolean :private, default: true
       t.integer :likes_count, default: 0
       t.integer :comments_count, default: 0
@@ -43,9 +42,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :username,             unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-    add_index :users, :username, unique: true
   end
 end
